76bf3d18e7a1210be8a0bd189af9bcede608694f
crist0 <crist0sant087@gmail.com>
2026-05-06 13:11:31 +0200

    feat: [Phase 2 - Task 2.2 & 2.3] - Implement regulation rugby lines with 2m run-off and responsive aspect-ratio

--- src/components/field/RugbyField.vue
+++ src/components/field/RugbyField.vue
@@ -1,14 +1,21 @@
 <template>
-  <div class="rugby-field" />
+  <div class="rugby-field">
+    <FieldLines />
+  </div>
 </template>
+
+<script setup>
+import FieldLines from '@/components/field/FieldLines.vue';
+</script>
 
 <style lang="scss" scoped>
 @use '@/assets/styles/variables.scss' as *;
 
 .rugby-field {
+  position: relative;
   width: 100%;
   max-height: 100%;
-  aspect-ratio: 100 / 70;
+  aspect-ratio: 124 / 74;
   background-color: $field-green;
 }
 </style>
